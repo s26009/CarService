625652665a7d8af1c6cc444e260b5d72751e40f6
Hleb Skachko <s26009@pjwstk.edu.pl>
2022-11-26 19:16:46 +0100

    added homework for test classes

--- src/test/java/com/s26009/carservice/CarServiceTests.java
+++ src/test/java/com/s26009/carservice/CarServiceTests.java
@@ -1,4 +1,23 @@
 package com.s26009.carservice;
 
+import org.junit.jupiter.api.Test;
+
 public class CarServiceTests {
+
+//    Do napisania w domu testy dla przypadków:
+//    Brak samochodu w CarStorage
+//    Samochód już wynajęty przez kogoś innego
+//    Data startDate jest późniejsza niż endDate
+//    Jaka jest cena wynajmu samochodu PREMIUM
+//    Jaka jest cena wynajmu samochodu STANDARD
+//    Co się stanie jeżeli vin będzie nullem
+
+    @Test
+    void shouldReturnRentalInfo_whenRentCar_givenExistingAvailableCarVin() {
+        //given
+
+        //when
+
+        //then
+    }
 }
--- src/test/java/com/s26009/carservice/CarTests.java
+++ src/test/java/com/s26009/carservice/CarTests.java
@@ -4,12 +4,4 @@ import org.junit.jupiter.api.Test;
 
 public class CarTests {
 
-    @Test
-    void shouldReturnRentalInfo_whenRentCar_givenExistingAvailableCarVin() {
-        //given
-
-        //when
-
-        //then
-    }
 }
