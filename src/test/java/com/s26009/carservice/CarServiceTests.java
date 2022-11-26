0ebceb78a1c5fb18e86612be5d99979cd5067ea4
Hleb Skachko <s26009@pjwstk.edu.pl>
2022-11-26 19:15:48 +0100

    created test classes

---
+++ src/test/java/com/s26009/carservice/CarServiceTests.java
@@ -0,0 +1,4 @@
+package com.s26009.carservice;
+
+public class CarServiceTests {
+}
---
+++ src/test/java/com/s26009/carservice/CarTests.java
@@ -0,0 +1,15 @@
+package com.s26009.carservice;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTests {
+
+    @Test
+    void shouldReturnRentalInfo_whenRentCar_givenExistingAvailableCarVin() {
+        //given
+
+        //when
+
+        //then
+    }
+}
